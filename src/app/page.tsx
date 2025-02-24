6544df9e04ef347eef6811d500ea42104de8d579
kyungmim <kyungmin727@gmail.com>
2025-02-24 23:37:56 +0900

    Style: 기본 레이아웃UI 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
 import { bookFetch } from '@/model/action/bookAction';
+import styles from '@styles/home.module.scss';
 
 const Home = async () => {
   const data = await bookFetch();
 
   console.log(data);
   return (
-    <div>
-      <h1>Book Search</h1>
-      <ul>
+    <div className={styles.homeWrapper}>
+      <div className={styles.header}>
+        <img src="/images/logo.svg" alt="logo" />
+        <h1>Romance Book Search</h1>
+      </div>
+      <input type="text" />
+      <ul className={styles.bookList}>
         {data?.map((item) => (
           <li key={item.lending_edition_s}>
-            <img src={`https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg`} alt="bookCover" />
+            <div className={styles.bookCover}>
+              <img src={`https://covers.openlibrary.org/b/id/${item.cover_i}-L.jpg`} alt="bookCover" />
+            </div>
             <h2>{item.title}</h2>
-            <p>{item.author_name[0]}</p>
-            <p>First Published: {item.first_publish_year}</p>
+            <p>
+              <span>First Published</span>: {item.first_publish_year}
+            </p>
+            <p>
+              <span>author_name</span>: {item.author_name[0]}
+            </p>
           </li>
         ))}
       </ul>
